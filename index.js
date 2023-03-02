40d15f5fc7b063b840c7d66bd8708c52184a0d1a
Motard80 <gaetan.wilbert@outlook.fr>
2023-03-02 15:15:43 +0100

    Bot fonctionnel
    Bot fonctionnel sans séparation (tout dans le meme fichier(le slashbulder ne fonctione pas

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
- const {Client, Collection}= require ("discord.js");
- const client = new Client({intents: 1});
- const {loadEvents}= require ("./Handlers/Events.js");
-const { Token}= require("./config.json");
+const {Client, ActionRowBuilder, InteractionType} = require('discord.js');
+const { Token}= require("./Config.json");
+const client= new Client({
+    intents:[3276799
+    ]
+})
+var prefix = "!";
+client.on('ready', () => {
+    console.log(` ${client.user.tag}suis connecter!`);
+});
+ client.on('messageCreate', async message => {
+if(message.content==  prefix +'Help') {
+    message.reply('Les commandes disponibles sont: ping, Help, Id, Name, ');
 
-Client.commands = new Collection();
-require("./Handlers/Commands")(client);
-require("./Handlers/Events")(client);
+}if(message.content==  prefix +'Play') {
+    message.reply('Le systeme de musique non disponible');
+}
+if(message.content==  prefix +'Id') {
+    message.reply(message.author.id, message.author.username)
+}
+if(message.content==  prefix +'Name') {
+    message.reply(message.author.username)
+}
+    /*
+if(message.content==  prefix +'Avatar') {
+
+message.reply(message.user.displayAvatarURL)
+
+}*/
+
+
+ })      
+ client.on('interactionCreate',  interaction => {
+    if(interaction.type== InteractionType.ApplicationCommand){
+        if(interaction.commandName == "pdp"){
+            interaction.channel.send(interaction.member.displayAvatarURL())
+        }
+    }
+ })     
  client.login(Token);
+          console.log(`Test`);  
